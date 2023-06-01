a1b8e2e46176fc542dad4a654e9022c6fb10bb5e
maria <pinina7773@gmail.com>
2023-06-01 23:42:11 +0300

    add gitignore

--- be-healthy2/be_healthy2/asgi.py
+++ be-healthy2/be_healthy2/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Be_healthy2 project.
+ASGI config for be_healthy2 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Be_healthy2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'be_healthy2.settings')
 
 application = get_asgi_application()
--- be-healthy2/manage.py
+++ be-healthy2/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Be_healthy2.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'be_healthy2.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
