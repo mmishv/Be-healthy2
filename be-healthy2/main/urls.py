4d175cd48e7b76d7acd6d8bd2bd2411b9e1a5c35
maria <pinina7773@gmail.com>
2023-06-03 00:49:50 +0300

    update main page urls

--- be-healthy2/main/urls.py
+++ be-healthy2/main/urls.py
@@ -4,6 +4,5 @@ from .views import FullArticleView
 
 urlpatterns = [
     path('', views.index, name='main'),
-    path('calculate/', views.calculate, name='calculate'),
     path('article/<int:article_id>/<slug:slug>', FullArticleView.as_view(), name='article details')
 ]
--- be-healthy2/main/views.py
+++ be-healthy2/main/views.py
@@ -6,13 +6,6 @@ from .forms import CalculatorForm
 from .models import Article
 
 
-def index(request):
-    return render(request, 'main/main.html', {
-        'form': CalculatorForm(),
-        'page': get_article_page(request),
-    })
-
-
 def get_article_page(request):
     articles = Article.objects.filter(moderated=True)
     paginator = Paginator(articles, 3)
@@ -21,21 +14,20 @@ def get_article_page(request):
     return page
 
 
-def calculate(request):
+def index(request):
+    form = CalculatorForm(request.GET)
     result = 0
-    if request.method == 'GET':
-        form = CalculatorForm(request.GET)
-        if form.is_valid():
-            sex = request.GET['sex']
-            weight = request.GET['weight']
-            height = request.GET['height']
-            age = request.GET['age']
-            activity = request.GET['activity']
-            goal = request.GET['goal']
-            result = 447.6 + 9.2 * float(weight) + 3.1 * float(height) - 4.3 * float(age) if sex == 'F' else \
-                88.36 + 13.4 * float(weight) + 4.8 * float(height) - 5.7 * float(age)
-            result *= float(activity)*float(goal)
-            result = int(result)
+    if form.is_valid():
+        sex = request.GET['sex']
+        weight = request.GET['weight']
+        height = request.GET['height']
+        age = request.GET['age']
+        activity = request.GET['activity']
+        goal = request.GET['goal']
+        result = 447.6 + 9.2 * float(weight) + 3.1 * float(height) - 4.3 * float(age) if sex == 'F' else \
+            88.36 + 13.4 * float(weight) + 4.8 * float(height) - 5.7 * float(age)
+        result *= float(activity) * float(goal)
+        result = int(result)
     return render(request, 'main/main.html', {
         'form': CalculatorForm(request.GET),
         'result': result,
