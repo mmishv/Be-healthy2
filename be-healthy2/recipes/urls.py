f032dac4c517fe40756675909c2f6b890747efa6
maria <pinina7773@gmail.com>
2023-06-29 17:11:09 +0300

    add create recipe view

--- be-healthy2/recipes/urls.py
+++ be-healthy2/recipes/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index, category
+from .views import index, category, RecipeCreateView
 
 urlpatterns = [
     path('', index, name='main recipes'),
     path('<int:category_id>/<slug:slug>', category, name='category'),
-    # path('create_recipe', RecipeCreate.as_view(), name='create_recipe'),
+    path('new_recipe', RecipeCreateView.as_view(), name='create_recipe'),
 ]
--- be-healthy2/recipes/views.py
+++ be-healthy2/recipes/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.generic import CreateView
 
+from recipes.forms import AddRecipeForm, IngredientFormSet
 from recipes.models import Recipe, RecipeCategory
 
 
@@ -24,3 +28,40 @@ def category(request, category_id, slug):
         'categories': RecipeCategory.objects.all(),
         'page': page,
     })
+
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    template_name = 'recipes/new_recipe.html'
+    form_class = AddRecipeForm
+    success_url = '/recipes'
+
+    def get(self, request, *args, **kwargs):
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        formset = IngredientFormSet()
+        return self.render_to_response(
+            self.get_context_data(form=form, formset=formset))
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        form = AddRecipeForm(request.POST, request.FILES)
+        formset = IngredientFormSet(request.POST)
+        if form.is_valid() and formset.is_valid():
+            return self.form_valid(form, formset, request.user)
+        else:
+            return self.form_invalid(form, formset)
+
+    def form_valid(self, form, formset, user):
+        self.object = form.save(commit=False)
+        self.object.author = user
+        self.object.save()
+        form.save_m2m()
+        formset.instance = self.object
+        formset.instance.user = self.request.user
+        formset.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form, formset):
+        return self.render_to_response(self.get_context_data(form=form, formset=formset))
