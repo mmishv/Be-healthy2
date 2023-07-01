884b20c4c06ad59386b57b5378c911113e4aa50c
maria <pinina7773@gmail.com>
2023-07-01 19:19:58 +0300

    add meal name editing

--- be-healthy2/diary/models.py
+++ be-healthy2/diary/models.py
@@ -3,7 +3,6 @@ from datetime import datetime, time
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 from django.db import models
-from django.utils import timezone
 
 from recipes.models import Product, UNIT_CHOICES
 
