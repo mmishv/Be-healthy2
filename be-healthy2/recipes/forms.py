3033f9e79c538fa49b3224455059481bc1073732
maria <pinina7773@gmail.com>
2023-07-03 21:04:52 +0300

    add init for CreateRecipeForm

--- be-healthy2/recipes/forms.py
+++ be-healthy2/recipes/forms.py
@@ -1,20 +1,27 @@
 from django import forms
 from django.forms import TextInput, Textarea, FileInput
-from django.forms.models import inlineformset_factory
 
+from diary.forms import custom_inlineformset_factory
 from .models import Recipe, Ingredient
 
-IngredientFormSet = inlineformset_factory(
+
+IngredientFormSet = custom_inlineformset_factory(
     Recipe, Ingredient, extra=1, can_delete=True, can_delete_extra=True, fields=('product', 'unit', 'quantity'),
     widgets={
-            'product': forms.Select(attrs={'class': 'form-control col-sm-6', 'required': 'true'}),
-            'unit': forms.Select(attrs={'class': 'form-control col-sm-2', 'required': 'true'}),
-            'quantity': forms.NumberInput(attrs={'class': 'form-control col-sm-2', 'required': 'true'}),
+        'product': forms.Select(attrs={'class': 'form-control col-sm-6', 'required': 'true'}),
+        'unit': forms.Select(attrs={'class': 'form-control col-sm-2', 'required': 'true'}),
+        'quantity': forms.NumberInput(attrs={'class': 'form-control col-sm-2', 'required': 'true'}),
     },
 )
 
 
-class AddRecipeForm(forms.ModelForm):
+class CreateRecipeForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        is_image_required = kwargs.pop('is_image_required', True)
+        super().__init__(*args, **kwargs)
+        self.fields['image'].required = is_image_required
+        self.fields['categories'].required = False
+
     class Meta:
         model = Recipe
         fields = ('title', 'categories', 'cooking_time', 'text', 'image')
@@ -22,8 +29,9 @@ class AddRecipeForm(forms.ModelForm):
             'title': TextInput(attrs={'placeholder': "Например, оладьи", 'class': "form-control", 'required': 'true'}),
             'cooking_time': TextInput(
                 attrs={'placeholder': "Например, 30", 'class': "form-control", 'required': 'true'}),
-            'image': FileInput(attrs={'class': 'form-control-file', 'required': 'true', 'accept': "image/png, image/jpeg"}),
+            'image': FileInput(
+                attrs={'class': 'form-control-file', 'accept': "image/png, image/jpeg"}),
             'text': Textarea(
                 attrs={'class': "form-control col-sm-8", 'required': 'true', 'rows': '5'}),
             'categories': forms.CheckboxSelectMultiple(),
-            }
+        }
